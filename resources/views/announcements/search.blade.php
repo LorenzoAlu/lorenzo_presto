370506151c839e1a12785f82c935523d3c5c7716
Lorenzo M. Alù <lorenzo.alu@gmail.com>
2021-04-05 12:18:10 +0200

    style site

--- resources/views/announcements/search.blade.php
+++ resources/views/announcements/search.blade.php
@@ -8,10 +8,13 @@
         </div>
 
         @if (count($announcements)==0)
-        <div class="row my-5 text-center">
-            <h3>Non ci sono annunci</h3>
+        <div class="row  text-center justify-content-center">
+            <div class="col-6 my-5">
+                <x-searchbar></x-searchbar>
+            </div>
+            <h3 class="my-5">Non ci sono annunci</h3>
             <div class="col-12">  
-            <img src="/media/20945436.jpg" class="img-fluid mx-auto w-50" alt="non-ci-sono-annunci">
+            <img src="/media/20945436.jpg" class="img-fluid mx-auto w-75 w-md-50" alt="non-ci-sono-annunci">
             
             </div>
             <div class="col-12">
@@ -24,7 +27,10 @@
         </div>
         @else 
 
-        <div class="row">
+        <div class="row justify-content-center">
+            <div class="col-6 mt-5 w-search-mobile">
+                <x-searchbar></x-searchbar>
+            </div>
             @foreach ($announcements as $announcement)
             <div class="col-12 col-md-4 my-5 p-5">
                 <x-card 
